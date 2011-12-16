ce2c3f1331c7089e5bc4519ff678c16ac57de919
Max Nikolenko <mix-player@yandex.ru>
2011-12-16 16:02:08 +0200

    Changes in View

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,5 @@
 class WelcomeController < ApplicationController
   def index
-     user_agent=request.user_agent
-
-    render 'index.html.haml', :locals => { :user_agent => request.user_agent }
+     render 'index.html.haml', :locals => { :user_agent => request.user_agent }
   end
 end

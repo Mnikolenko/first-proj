c92e7a7a7c50f9e92a2d1a42d93a4859b9527e7b
Max Nikolenko <mix-player@yandex.ru>
2011-12-22 18:59:50 +0200

    migration works

--- db/migrate/20111220171345_create_posts.rb
+++ db/migrate/20111220171345_create_posts.rb
@@ -2,7 +2,7 @@ class CreatePosts < ActiveRecord::Migration
   def change
     create_table :posts do |t|
       t.string :title
-      t.srting :text
+      t.string :text
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111219154619) do
+ActiveRecord::Schema.define(:version => 20111220171345) do
+
+  create_table "posts", :force => true do |t|
+    t.string   "title"
+    t.string   "text"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", :force => true do |t|
     t.string   "user_email"
